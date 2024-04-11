18537e2f546e5213a90a0cc2ee622f5928d6c5e6
Rodrigo Saes Juan <rodrigo.juan@gmail.com>
2024-04-11 13:14:39 -0300

    config: vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hostname: 'localhost'
+    },
     plugins: [
         laravel({
             input: [
